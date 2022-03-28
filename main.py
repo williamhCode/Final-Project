d17990be7bcb64a940cb56040cb4a8506d4db439
williamhCode <83525937+williamhCode@users.noreply.github.com>
2022-03-28 11:30:33 +0800

    setup pygame

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+
+WIDTH = 1280
+HEIGHT = 720
+
+# Setup pygame/window ----------------------------------- #
+# pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+running = True
+while running:
+    # Events ------------------------------------------------- #
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                
+    # Background --------------------------------------------- #
+    screen.fill((100, 0, 0))
+    
+    # Update ------------------------------------------------- #
+    pygame.display.flip()
+            
+            
