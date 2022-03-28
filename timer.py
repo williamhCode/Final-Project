b2da867e1eef3ea3b43031aa7394c15f14a90133
williamhCode <83525937+williamhCode@users.noreply.github.com>
2022-03-28 11:37:12 +0800

    tiemr

--- timer.py
+++ timer.py
@@ -6,15 +6,14 @@ class Timer:
         self.time = time.perf_counter()
         self.unprocessed = 0
         self.prev_time = time.perf_counter()
-        self.fps_list = []
-        
-        self.first_tick = True
+        self.fps_list = [0]
         
     def tick(self, fps=None):
         if fps != None:
             frame_cap = 1 / fps
             
             while True:
+                
                 if self.unprocessed >= frame_cap:
                     self.unprocessed -= frame_cap
                     break
@@ -23,12 +22,16 @@ class Timer:
                 self.unprocessed += time_2 - self.time
                 self.time = time_2
                 
+            if self.unprocessed > frame_cap:
+                self.unprocessed = frame_cap
+                
         dt = time.perf_counter() - self.prev_time
         self.prev_time = time.perf_counter()
         
-        self.fps_list.append(1 / dt)
-        if len(self.fps_list) > 10:
-            self.fps_list.pop(0)
+        if dt != 0:
+            self.fps_list.append(1 / dt)
+            if len(self.fps_list) > 50:
+                self.fps_list.pop(0)
                 
         return dt
     
